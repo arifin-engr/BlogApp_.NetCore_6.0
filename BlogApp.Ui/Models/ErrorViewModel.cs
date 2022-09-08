0296d9717741e0732e3609b08830d8e8500fef90
ARIFIN <ARIFIN@DESKTOP-B5K8CDM>
2022-09-08 13:07:44 +0600

    Add project files.

---
+++ BlogApp.Ui/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BlogApp.Ui.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
